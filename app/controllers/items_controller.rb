b702c93d009339f198835604fcfa20d49cca9b3c
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-02-04 21:22:07 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :image, :description, :category_id, :condition_id,
+      :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price
+    ).merge(user_id: current_user.id)
+  end
 end
