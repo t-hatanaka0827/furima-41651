6b620cb8843e1cbf3ecc0bc1d84080755924aeb6
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-02-05 21:44:22 +0900

    ActiveHashを利用するモデルクラスの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
 
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_area
+  belongs_to :shipping_day
 end
