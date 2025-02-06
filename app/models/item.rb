7e912115b91da4d32c33f05894dd1e3681eecf7a
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-02-06 20:56:17 +0900

    バリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,20 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :shipping_area
   belongs_to :shipping_day
+
+  validates :image, presence: true
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id,     numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_day_id,  numericality: { other_than: 1, message: "can't be blank"}
+
+  validates :price, numericality: { only_integer: true, message: "は半角数字で入力してください" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "は¥300~¥9,999,999の間で入力してください" }
 end
--- db/migrate/20250203122424_create_items.rb
+++ db/migrate/20250203122424_create_items.rb
@@ -5,8 +5,8 @@ class CreateItems < ActiveRecord::Migration[7.1]
       t.text       :description,       null: false
       t.integer    :category_id,       null: false
       t.integer    :condition_id,      null: false
-      t.integer    :shipping_cost_id,   null: false
-      t.integer    :shipping_area_id,     null: false
+      t.integer    :shipping_cost_id,  null: false
+      t.integer    :shipping_area_id,  null: false
       t.integer    :shipping_day_id,   null: false
       t.integer    :price,             null: false
       t.references :user,              null: false, foreign_key: true
