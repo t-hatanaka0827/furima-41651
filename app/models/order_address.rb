9839130afd50f2620404c7944f6802e4a1b96811
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-02-20 00:10:21 +0900

    attr_accessor記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,7 @@
 class OrderAddress < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :building, :phone_number, :token
+
+  def save
+  end
 end
