07cb3083ba095b9560349066262e55f519d25944
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-02-23 11:23:12 +0900

    ルーティング記述

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,24 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :shipping_area_id, :city, :address, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+end
