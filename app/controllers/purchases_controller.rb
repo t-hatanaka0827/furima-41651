483a8d9731c43f9107c7097f67da900cb0d9c1cf
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-03-01 18:25:56 +0900

    JavaScriptで公開鍵を読み込み

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,12 +4,14 @@ class PurchasesController < ApplicationController
   before_action :redirect_if_invalid
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+       pay_item
        @order_address.save
       redirect_to root_path
     else
@@ -34,4 +36,14 @@ class PurchasesController < ApplicationController
     end
   end
 
+  def pay_item
+   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+   Payjp::Charge.create(
+    amount: order_params[:price],
+    card: order_params[:token],
+    currency: 'jpy'
+  )
+  end
+
 end
+
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-
-  const payjp = Payjp('pk_test_pk_test_05c07892b5e21c1ae14a9354')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
